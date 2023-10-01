b8526e5fd53e350d61f55a254467f9cf62c4592b
VictorChukwudi <victorukay@gmail.com>
2023-10-01 07:10:39 +0100

    cors add

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 const connectDB = require("./config/db");
 const videoRoutes = require("./routes/videos.routes");
 
 connectDB();
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 const path = require("path");
 
 console.log(path.resolve(__dirname, `./videos`));
+console.log(__dirname);
